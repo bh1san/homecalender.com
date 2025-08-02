ae7a19f405a202139dafd17bfd81efc5f6b668c2
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-08-02 19:31:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet"></link>
       </head>
-      <body>
+      <body className="min-h-screen bg-muted/40 font-body">
         {children}
         <Toaster />
       </body>
--- src/app/template.tsx
+++
@@ -1,5 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-muted/40 font-body">{children}</div>
-  )
-}
