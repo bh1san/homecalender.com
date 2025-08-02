ee41393f1060f5a43d649593308db06a5875a2fd
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-08-02 17:58:01 +0000

    or use this to view the calendar: <iframe src="https://nepalicalendar.ra

--- src/components/nepali-calendar-widget.tsx
+++ src/components/nepali-calendar-widget.tsx
@@ -6,12 +6,12 @@ const NepaliCalendarWidget = () => {
   return (
     <div className="w-full h-full">
       <iframe
-        src="https://www.nepalicalendar.com/widgets/full"
+        src="https://nepalicalendar.rat32.com/embed.php"
         frameBorder="0"
         scrolling="no"
         marginWidth={0}
         marginHeight={0}
-        style={{ border: 'none', overflow: 'hidden', width: '100%', height: '425px' }}
+        style={{ border: 'none', overflow: 'hidden', width: '100%', height: '650px', borderRadius: '5px', padding: '0px', margin: '0px' }}
         allowTransparency={true}
       ></iframe>
     </div>
