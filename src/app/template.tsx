a388142e1350769f857dcf105fee3f6c915b176d
Chhatradev Chaudhary <bhisan25@gmail.com>
2025-08-02 19:21:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,11 +1,6 @@
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet"></link>
-      </head>
       <div className="min-h-screen bg-muted/40 font-body">{children}</div>
     </>
   )
